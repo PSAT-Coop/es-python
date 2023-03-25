ded1c2dc0185a56a79636278a602ed6bed998d10
junseokim0103 <hazzisss5249@gmail.com>
2023-03-25 21:17:52 +0900

    es_test_re

--- test_es.py
+++ test_es.py
@@ -1,13 +1,16 @@
+### elastic_search + python test code
+
 
 from elasticsearch import Elasticsearch
 
 # Elasticsearch 서버에 연결
-es = Elasticsearch(["http://localhost:9200"])
+es_host = "http://localhost:9200"
+es = Elasticsearch([es_host]) ##자신의 로컬 내 es 주소를 적음 된다.
 
 # Elasticsearch 버전 확인
 print(es.info())
 
-# 인덱스 생성
+# 인덱스 생성 -> 인덱스가 이미 존재할 경우에(여기서는  my_index) 실행되지 않게
 if not es.indices.exists(index='my_index'):
         es.indices.create(index='my_index')
 
