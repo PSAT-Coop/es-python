0ec6c01d7d5249b43721c3676d3c09e03610dad1
junseokim0103 <hazzisss5249@gmail.com>
2023-03-25 20:49:48 +0900

    es-python-test

---
+++ test_es.py
@@ -0,0 +1,21 @@
+
+from elasticsearch import Elasticsearch
+
+# Elasticsearch 서버에 연결
+es = Elasticsearch(["http://localhost:9200"])
+
+# Elasticsearch 버전 확인
+print(es.info())
+
+# 인덱스 생성
+if not es.indices.exists(index='my_index'):
+        es.indices.create(index='my_index')
+
+        # 문서 추가
+        doc = {'title': 'Elasticsearch Python Test', 'content': 'This is a test document.'}
+        es.index(index='my_index', body=doc)
+
+        # 검색
+        res = es.search(index='my_index', body={'query': {'match': {'content': 'test'}}})
+        print(res['hits']['hits'])
+
